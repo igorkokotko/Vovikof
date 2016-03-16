197e06d3c180df40111b277626d13929cc640cad
igorkokotko <thylacosmilu@gmail.com>
2016-03-16 17:50:55 +0000

    video background

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(window).load(function() {
 	$(".loader").fadeOut(5500);
 })
+$(function() {
+    var BV = new $.BigVideo({useFlashForFirefox:false});
+	BV.init();
+    BV.show([
+        { type: "video/mp4",  src: "vids/river.mp4" },
+        { type: "video/webm", src: "vids/river.webm" },
+        { type: "video/ogg",  src: "vids/river.ogv" }
+    ]);
+});
